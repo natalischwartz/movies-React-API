87f9892916a82b33fe9e79aeb89e65ff72314eb6
Natali Schwartz <schwartznatali@gmail.com>
2024-12-10 17:04:46 +0100

    create styles for the components

--- src/pages/PeliculaDetalle.jsx
+++ src/pages/PeliculaDetalle.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { get } from "../utils/httpCliente"
-
-
+import './PeliculaDetalle.css'
 
 export const PeliculaDetalle = () => {
-
     const { peliculaId } = useParams()
-
     const [pelicula, setPelicula] = useState(null)
 
     useEffect(() => {
@@ -15,30 +12,29 @@ export const PeliculaDetalle = () => {
             console.log(data)
             setPelicula(data)
         })
-
     }, [peliculaId])
 
     if (!pelicula) {
         return null
     }
-
     const imgUrl = `https://image.tmdb.org/t/p/w200${pelicula.poster_path}`
-
-
     return (
         <div className="contenedorDetalle">
             <img className="col" src={imgUrl} alt={pelicula.title} />
             <div className="peliculaDetalle col">
-                <p className="item"><strong>Titulo:</strong>{pelicula.title}</p>
+                <p className="item">
+                    <strong>Titulo:</strong>
+                    {pelicula.title}
+                </p>
+                <p>
+                    <strong>Géneros:</strong>
+                    {pelicula.genres.map((genre)=> genre.name).join(",")}
+                </p>
                 <p>
                     <strong>Descripción:</strong>
                     {pelicula.overview}
                 </p>
-
             </div>
-
         </div>
-
     )
-
 }
