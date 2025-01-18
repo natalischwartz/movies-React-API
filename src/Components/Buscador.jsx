6789026fa06d206cd4b89af3882c394c9499de80
Natali Schwartz <schwartznatali@gmail.com>
2025-01-18 18:38:36 +0100

    Add a function to the search engine to bring up the matches

--- src/Components/Buscador.jsx
+++ src/Components/Buscador.jsx
@@ -1,17 +1,36 @@
 import { FaSearch } from "react-icons/fa";
+import {useNavigate} from 'react-router-dom' //7. importamos useNavigate
 import './Buscador.css'
+import { useState } from "react"; //1.importar useState
 
 export const Buscador = () => {
+
+ 
+    const navigate = useNavigate() //8.defino la variable 
+
+    const [palabraBuscada, setPalabraBuscada] = useState("") //2.controlamos el estado del input
+
+
+    //3. realizamos la funcion para controlar el submit. el envio de info( cuando apretamos el boton buscar)
+    const handleSubmit = (e) =>{
+        e.preventDefault(); // 5. no se realiza el submit.
+        navigate(`/?search=${palabraBuscada}`) //9. enviar lo que se escribe en el input a la URL.
+    }
 
 
 
 
 
     return(
-      <form className="containerBuscador">
+      <form className="containerBuscador" onSubmit={handleSubmit}> {/*4.ponemos el evento en el form*/}
         <div className="cajaBuscador">
 
-            <input type="text"
+            <input 
+            value={palabraBuscada}
+            //6.Mostramos como controlamos un input
+            onChange={(e)=> setPalabraBuscada(e.target.value)}
+            
+            type="text"
             className="inputBuscador"
             
             
--- src/Components/PeliculasGrid.jsx
+++ src/Components/PeliculasGrid.jsx
@@ -4,22 +4,46 @@ import { useState , useEffect } from "react"
 import { PeliculasCard } from "./PeliculasCard"
 import "./PeliculasGrid.css"
 import { Spinner } from "./Spinner.jsx"
+import { useLocation } from "react-router-dom"; //10. importamos useLocation 
 
 
 
 export const PeliculasGrid = () =>{
 
+ //13. utilizamos este hook para tomar lo que viene por parametro.
+ const useQuery = () =>{
+    return new URLSearchParams(useLocation().search)
+  }
+
+  //12. utilizo useQuery() para obtener lo que buscamos
+  const query = useQuery()
+  const search = query.get("search")
+  // console.log(search)
+
+  //11. vemos en el navegador lo que vaiaja en la URL( query params), separamos lo que buscamos (location.search)
+  // const location = useLocation()
+  // console.log(location);
+  // console.log(location.search)
+
+
+
 const [peliculas, setPeliculas] = useState([]);
 const [cargando, setCargando] = useState(true);
 
 useEffect(() =>{
+    
     setCargando(true)
-    get("/discover/movie").then((data)=>{
+    //15. hago un if ternario, si hay busqueda asigna ese valor a una url sino muestra todas las peliculas
+
+    const searchURL  = search?"/search/movie?query=" + search:"/discover/movie"
+
+
+    get(searchURL).then((data)=>{
         console.log(data)
         setPeliculas(data.results)
     setCargando(false)    
     })
-},[])
+},[search]) //14.agregar search
 
 if(cargando){
     return <Spinner/>
